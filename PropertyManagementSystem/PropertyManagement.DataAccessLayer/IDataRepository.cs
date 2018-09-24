ca63bad70355bddb41f4811a7c5d3f7be0ce40c1
kazi <kazi@instawize.com>
2018-09-24 18:05:53 -0400

    ok

---
+++ PropertyManagementSystem/PropertyManagement.DataAccessLayer/IDataRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManagement.DataAccessLayer
+{
+    public interface IDataRepository<T>
+    {
+        IList<T> GetAllList(params Expression<Func<T, object>>[] expressions);
+        IList<T> GetSelectedList();
+
+        T GetSingle();
+
+        void Add(params T[] items);
+        void Update(params T[] items);
+        void Remove(params T[] items);
+
+
+
+    }
+}
